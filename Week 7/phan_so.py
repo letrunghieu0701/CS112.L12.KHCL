d9a45862b98f5e54a5ddab9e37a3ed3d0afed55f
Dang Hoang Minh <43031977+danghoangminh@users.noreply.github.com>
2021-01-04 13:17:17 +0700

    Last version

--- Week 7/phan_so.py
+++ Week 7/phan_so.py
@@ -5,12 +5,12 @@ b = int(input())
 c = int(input())
 d = int(input())
 
-if(a*d < b*c):
+if(a*d < c*b):
     number_of_steps = 0
-    while (a*d <= b*c):
-        g = math.gcd(a + 1, b + 1)  # tìm ước chung của a++ và b++ để tìm xem có thể tối giản phân số đc ko
-        a = (a + 1)//g              # nếu g > 1 => có thể tối giản được
-        b = (b + 1)//g
+    while (a*d < c*b):
+        g = math.gcd(a+1, b+1)
+        a = (a+1) // g
+        b = (b+1) // g
         number_of_steps += 1
         if (a == c and b == d):
             print(number_of_steps)
